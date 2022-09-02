067722b16c2b169baadc904bc4fca021874e9618
HyuncheolKim2021 <hyuncheol_kim@bita.jp>
2022-09-02 14:58:31 +0900

    benchmark: 環境変数の読み込み, toml, json, yml

--- config/config.go
+++ config/config.go
@@ -2,27 +2,35 @@ package config
 
 import (
 	_ "embed"
+	"encoding/json"
 	"errors"
 	"github.com/pelletier/go-toml/v2"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 	"strings"
 )
 
 var (
 	//go:embed environments/local.toml
 	Local []byte
+	//go:embed environments/local.json
+	LocalJson []byte
+	//go:embed environments/local.yml
+	LocalYaml []byte
 	//go:embed environments/development.toml
 	Development []byte
 )
 
 type Config struct {
-	Stage    string
-	Database struct {
-		Port     int
-		User     string
-		Password string
-		DbName   string
-	}
+	Stage    string `json:"stage" yaml:"stage"`
+	Database Database
+}
+
+type Database struct {
+	Port     int    `json:"port" yaml:"port"`
+	User     string `json:"user" yaml:"user""`
+	Password string `json:"password" yaml:"password"`
+	DbName   string `json:"dbname" yaml:"dbname"`
 }
 
 func LoadConfigWithViper(flag string) (*Config, error) {
@@ -63,6 +71,36 @@ func LoadConfigWithEmbed(flag string) (*Config, error) {
 		return &localConfig, nil
 	case "development":
 		return &developmentConfig, nil
+	default:
+		return nil, errors.New("could not load config")
+	}
+}
+
+func LoadConfigJsonWithEmbed(flag string) (*Config, error) {
+	var localConfig Config
+	err := json.Unmarshal(LocalJson, &localConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	switch flag {
+	case "local":
+		return &localConfig, nil
+	default:
+		return nil, errors.New("could not load config")
+	}
+}
+
+func LoadConfigYamlWithEmbed(flag string) (*Config, error) {
+	var localConfig Config
+	err := yaml.Unmarshal(LocalYaml, &localConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	switch flag {
+	case "local":
+		return &localConfig, nil
 	default:
 		return nil, errors.New("could not load config")
 	}
