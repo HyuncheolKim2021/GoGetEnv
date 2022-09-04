476c4da0e8c7d5a9ab7feadcf9cd406c484d6d41
HyuncheolKim2021 <hyuncheol_kim@bita.jp>
2022-09-04 12:55:51 +0900

    fix: b.Nを用いたベンチマークに変更

--- config/config_test.go
+++ config/config_test.go
@@ -1,40 +1,48 @@
 package config_test
 
 import (
-	"reflect"
 	"testing"
 	cfg "vipersample/config"
 )
 
 func BenchmarkLoadConfigWithEmbed(b *testing.B) {
 	b.ResetTimer()
-	dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
-	b.Run("load config from toml", func(b *testing.B) {
-		config, _ := cfg.LoadConfigWithEmbed("local")
-		if !reflect.DeepEqual(&config.Database, dbConfig) {
-			b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
-		}
-	})
+	//dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
+	for i := 0; i < b.N; i++ {
+		b.Run("load config from toml", func(b *testing.B) {
+			//config, _ :=
+			cfg.LoadConfigWithEmbed("local")
+			//if !reflect.DeepEqual(&config.Database, dbConfig) {
+			//	b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
+			//}
+		})
+	}
 }
 
 func BenchmarkLoadConfigJsonWithEmbed(b *testing.B) {
 	b.ResetTimer()
-	dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
-	b.Run("load config from json", func(b *testing.B) {
-		config, _ := cfg.LoadConfigJsonWithEmbed("local")
-		if !reflect.DeepEqual(&config.Database, dbConfig) {
-			b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
-		}
-	})
+	//dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
+	for i := 0; i < b.N; i++ {
+		b.Run("load config from json", func(b *testing.B) {
+			//config, _ :=
+			cfg.LoadConfigJsonWithEmbed("local")
+			//if !reflect.DeepEqual(&config.Database, dbConfig) {
+			//	b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
+			//}
+		})
+	}
 }
 
 func BenchmarkLoadConfigYamlWithEmbed(b *testing.B) {
 	b.ResetTimer()
-	dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
-	b.Run("load config from json", func(b *testing.B) {
-		config, _ := cfg.LoadConfigYamlWithEmbed("local")
-		if !reflect.DeepEqual(&config.Database, dbConfig) {
-			b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
-		}
-	})
+	//dbConfig := &cfg.Database{Port: 5432, User: "viper_sample_user", Password: "password", DbName: "viper_development"}
+	for i := 0; i < b.N; i++ {
+		b.Run("load config from json", func(b *testing.B) {
+			//config, _ :=
+			cfg.LoadConfigYamlWithEmbed("local")
+			//if !reflect.DeepEqual(&config.Database, dbConfig) {
+			//	b.Errorf("diff got config: %v, want: %v", config.Database, dbConfig)
+			//}
+		})
+	}
 }
